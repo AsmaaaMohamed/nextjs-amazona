8206b199f1ccfc5d6e8132c0960e9dc376e0e263
Asmaa Mohamed <eng_asmaaa20102000@yahoo.com>
2025-08-13 21:10:30 +0300

    create-browsing-history

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,6 +9,7 @@ import {
 import { toSlug } from "@/lib/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductSlider from "@/components/shared/product/product-slider";
+import BrowsingHistoryList from "@/components/shared/browsing-history-list";
 
 export default async function HomePage() {
   const categories = (await getAllCategories()).slice(0, 4);
@@ -84,6 +85,9 @@ export default async function HomePage() {
             />
           </CardContent>
         </Card>
+      </div>
+      <div className="p-4 bg-background">
+        <BrowsingHistoryList />
       </div>
     </>
   );
