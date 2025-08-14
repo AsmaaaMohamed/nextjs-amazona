e77ad1857ffe356945d10ff80058eb8c79c15690
Asmaa Mohamed <eng_asmaaa20102000@yahoo.com>
2025-08-14 19:38:20 +0300

    pay-order-by-paypal

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
 export const formatNumberWithDecimal = (num: number): string => {
   const [int, decimal] = num.toString().split('.')
   return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int
 }
-// PROMPT: [ChatGTP] create toSlug ts arrow function that convert text to lowercase, remove non-word, non-whitespace, non-hyphen characters, replace whitespace, trim leading hyphens and trim trailing hyphens
+// PROMPT: [ChatGTP] create toSlug ts arrow function that convert text to lowercase, remove non-word,
+// non-whitespace, non-hyphen characters, replace whitespace, trim leading hyphens and trim trailing hyphens
 
 export const toSlug = (text: string): string =>
   text
@@ -31,11 +33,13 @@ const NUMBER_FORMATTER = new Intl.NumberFormat('en-US')
 export function formatNumber(number: number) {
   return NUMBER_FORMATTER.format(number)
 }
+
 export const round2 = (num: number) =>
   Math.round((num + Number.EPSILON) * 100) / 100
 
 export const generateId = () =>
   Array.from({ length: 24 }, () => Math.floor(Math.random() * 10)).join('')
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const formatError = (error: any): string => {
   if (error.name === 'ZodError') {
@@ -60,6 +64,7 @@ export const formatError = (error: any): string => {
       : JSON.stringify(error.message)
   }
 }
+
 export function calculateFutureDate(days: number) {
   const currentDate = new Date()
   currentDate.setDate(currentDate.getDate() + days)
@@ -128,4 +133,8 @@ export const formatDateTime = (dateString: Date) => {
     dateOnly: formattedDate,
     timeOnly: formattedTime,
   }
+}
+
+export function formatId(id: string) {
+  return `..${id.substring(id.length - 6)}`
 }
