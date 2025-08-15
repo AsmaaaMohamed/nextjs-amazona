8d7522f05e5cb7bac36a15816558887fafbda403
Asmaa Mohamed <eng_asmaaa20102000@yahoo.com>
2025-08-15 13:10:16 +0300

    fix

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 import { sendPurchaseReceipt } from '@/emails'
 import Order from '@/lib/db/models/order.model'
-import User from '@/models/User'
+import User from '@/lib/db/models/user.model'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export async function POST(req: NextRequest) {
